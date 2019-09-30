d9847dd3e2dd54405b73f706e7d2b4357c69f446
Michael Mardahl <iphase@iphase.dk>
2019-09-30 10:27:45 +0200

    Update and rename invoke-PushSync to invoke-PushSync.ps1

--- invoke-PushSync.ps1
+++ invoke-PushSync.ps1
@@ -2,6 +2,7 @@
 <#
   A simpler approach to initiating an MDM sync, as explained by 
   @mniehaus in his blog post https://oofhours.com/2019/09/28/forcing-an-mdm-sync-from-a-windows-10-client/
+  This method should be run as SYSTEM or an administrative account.
 #>
 
 Start-ScheduledTask -TaskName "PushLaunch"
